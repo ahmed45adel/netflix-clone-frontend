ac69602faffb211593a21e2da32caaf8c99a5bb6
Ahmed Adel <ahmed45adel@yahoo.com>
2025-07-28 08:38:37 +0300

    fix loading state

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useAuthStore } from "../store/authUser";
 
 const LoginPage = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
+	const { login, isLoggingIn } = useAuthStore();
 
 	const handleLogin = (e) => {
 		e.preventDefault();
