263b874f39d7303b1d7ab02e13da0fdb41cc66d3
Ahmed Adel <ahmed45adel@yahoo.com>
2025-07-29 09:05:13 +0300

    include token in axios requests

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,11 +1,25 @@
 import axios from "axios";
+import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development"
-		? "http://localhost:5000"
-		: `${import.meta.env.VITE_API_BASE_URL}`,
-	withCredentials: true, // send cookies to the server
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:5000"
+    : `${import.meta.env.VITE_API_BASE_URL}`,
+  withCredentials: true, // send cookies to the server
 });
 
+// Add a request interceptor
+axiosInstance.interceptors.request.use(
+  config => {
+    const token = Cookies.get('jwt-netflix');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
